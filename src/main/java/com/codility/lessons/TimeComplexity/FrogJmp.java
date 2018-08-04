fd7532a304e8b02ea48508f41140d471eaa202fa
Nurrahman Hadi <nurrahmanhaadii@gmail.com>
2018-08-04 11:21:24 +0700

    Update FrogJmp.java

--- src/main/java/com/codility/lessons/TimeComplexity/FrogJmp.java
+++ src/main/java/com/codility/lessons/TimeComplexity/FrogJmp.java
@@ -5,6 +5,22 @@ public class FrogJmp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(new FrogJmp().solution2(10, 85, 30));
+		//System.out.println(Integer.MAX_VALUE);
+		//System.out.println(Integer.MIN_VALUE);
+
+	}
+
+
+	public int solution(int X, int Y, int D) {
+		// write your code in Java SE 7
+		int wholeValue;
+		if(X==Y)
+			return 0;
+		wholeValue=(int) Math.ceil((double)(Y-X)/D);
+		return wholeValue;
+
+
 	}
 
 }
